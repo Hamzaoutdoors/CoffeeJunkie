db816a3419d4c4545b7a0bcae7b6eef084d9c6f7
Hamza <hamzaoutdoors@gmail.com>
2021-09-14 01:32:49 +0100

    Updates

---
+++ precache-manifest.a279ffedb17041b48872717fcbabdcc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7cc473bca5112a869df9541efeb5c8",
+    "url": "/CoffeeJunkie/index.html"
+  },
+  {
+    "revision": "b76d8609666515930dc4",
+    "url": "/CoffeeJunkie/static/css/main.24cf5213.chunk.css"
+  },
+  {
+    "revision": "cb9d692760b4a984f8c5",
+    "url": "/CoffeeJunkie/static/js/2.ddfbfb68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CoffeeJunkie/static/js/2.ddfbfb68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76d8609666515930dc4",
+    "url": "/CoffeeJunkie/static/js/main.dc299d6c.chunk.js"
+  },
+  {
+    "revision": "23d78117a0eb285201d2",
+    "url": "/CoffeeJunkie/static/js/runtime-main.6e980c20.js"
+  },
+  {
+    "revision": "1151141c0dd3f3b8e1969fa0af3648ea",
+    "url": "/CoffeeJunkie/static/media/logoCoffee.1151141c.svg"
+  }
+]);
